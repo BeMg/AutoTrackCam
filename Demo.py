402b5796231a4b66686434e237b9eb1f3bfca212
BeMg <gccbg04538@gmail.com>
2017-08-20 13:26:44 +0800

    add HOG

--- Demo.py
+++ Demo.py
@@ -2,7 +2,7 @@ import cv2
 from GetVideoFromCam import GetVideoFromCam
 from utils import draw
 from DetectPeople import DetectPeople
-from HOG import HOGdetect
+from DetectBody import DetectBody
 
 
 cap = GetVideoFromCam(0)
@@ -10,7 +10,13 @@ cap = GetVideoFromCam(0)
 while True:
     flag, frame = cap.read()
     
+    rects = DetectBody(frame)
     
+    # print(rects)
+
+    if len(rects) > 0:
+        draw(frame, rects, (255, 0, 0))
+
     cv2.imshow('GGG', frame)
     if cv2.waitKey(5) == 27:
         cap.release()
--- DetectBody.py
+++ DetectBody.py
@@ -1,20 +1,16 @@
 from __future__ import print_function
-from imutils.object_detection import non_max_suppression
-from imutils import paths
-import numpy as np
-import imutils
 import cv2
-
+import numpy as np
 
 def DetectBody(frame):
 
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-    (rects, weight) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.1)
+    (rects, weight) = hog.detectMultiScale(frame)
     
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    # pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
     return rects
     
