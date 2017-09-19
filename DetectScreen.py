afb338fd31c8c577b22386ea72dc77363b6e4273
Eason Huang <hchusiang@csie.io>
2017-09-19 20:44:58 +0800

    Rewrite DetectScreen.py(Not tested yet)

--- DetectScreen.py
+++ DetectScreen.py
@@ -3,65 +3,33 @@ import numpy as np
 from detectanddraw import draw
 from TurningWithDetect import Turn
 
-def PolygonArea(corners):
-    #n = len(corners) # of corners
-    #print(corners)
-    #print(corners.shape)
-    l, m, n = corners.shape
-    area = 0.0
-    for i in range(l):
-        j = (i + 1) % l
-        area += corners[i][0][0] * corners[j][0][1]
-        area -= corners[j][0][0] * corners[i][0][1]
-    area = abs(area) / 2.0
-    return area
+
+def calculateRect(contour, size):
+
+    contour = contour[np.argsort(contour[:, 0])]
+    contour = contour[np.argsort(contour[:, 1])]
+
+    leftTop = contour[0]
+    rightDown = contour[len(contour)]
+
+    rect = [leftTop, rightDown]
+
+    return rect
+    
 
 def selectScreen(contours):
-    x = np.array([[1, 2]])
-    toReturn = contours[0]
-    left = np.array([[]])
-    right = np.array([[]])
-    maxArea = -1.0
-    for c in contours:
-        area = PolygonArea(c)
-        if(area > maxArea):
-            maxArea = area
-            toReturn = c
-    #print(np.array_repr(toReturn).replace('\n', ''))
-    l, m, n = toReturn.shape
-    toReturn = toReturn.reshape(l, n)
-    #print(np.array_repr(toReturn).replace('\n', ''))
-    lx = 640
-    ly = 480
-    rx = -1
-    ry = -1
-    for i in toReturn:
-        if(i[0] < lx): lx = i[0]
-        if(i[1] < ly): ly = i[1]
-        if(i[0] > rx): rx = i[0]
-        if(i[1] > ry): ry = i[1]
 
-    return c, [lx, ly], [rx, ry]
+    area = -1.0
+    result = contours[0]
 
-def screenState(left, right, length, height):
-    stateBoundary = 0 # 0 for vaild, 1 for out of left boundary, 2 for out of right boundary
-    stateVisible = 0 # 0 for vaild , 1 for out of visible
-    leftBound = length * 0.1
-    rightBound = length * 0.9
-    if(left[0] < leftBound):
-        print("Screen out of the left boundary")
-       # Turn(2, 1) 
-        stateBoundary = 1
-    elif(right[0] > rightBound):
-        print("Screen out of the right boundary")
-       # Turn(2, 0)
-        stateBoundary = 2
-    if( (right[0] - left[0]) * (right[1] - left[1]) < length * height * 0.4):
-        print("Screen isn't visvible, Do the operation")
-        stateVisible = 1
-    if(stateBoundary == 0 and stateVisible == 0):
-        print("Maintain the state")
-    return [stateBoundary, stateVisible]
+    for contour in contours:
+        curr = cv2.contourArea(contour)
+        if curr > area:
+            curr = area
+            result = contour
+
+        return result
+
 
 
 def DetectScreen(frame):
@@ -71,18 +39,10 @@ def DetectScreen(frame):
     ret, binarythresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY +cv2.THRESH_OTSU)
 
     img2, contours, hierarchy = cv2.findContours(binarythresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    coordLeft = []
-    coordRight = []
 
     if hierarchy is None:
         return [] 
 
-    screen, coordLeft, coordRight = selectScreen(contours)
+    rect = selectScreen(contours)
 
-    result = list()
-    result.append(coordLeft[0])
-    result.append(coordLeft[1])
-    result.append(coordRight[0])
-    result.append(coordRight[1])
-
-    return result
+    return rect
