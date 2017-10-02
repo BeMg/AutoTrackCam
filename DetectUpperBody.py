5149012d9ff7eab507c87172b7a7a89ff7b2ceea
BeMg <gccbg04538@gmail.com>
2017-10-02 16:26:46 +0800

    add DetectUpperBody

---
+++ DetectUpperBody.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import pickle
+from utils import DetectMymethod2
+from sklearn import svm
+
+with open('./svm_model/upperbody64x40', 'rb') as fp:
+    clf = pickle.load(fp)
+
+
+W = 40
+H = 64
+
+hog = cv2.HOGDescriptor((W, H), (16, 16), (8,8), (8,8), 9)
+
+
+def DetectUpperBody(img):
+    rect = DetectMymethod2(gray, H, W, 3.5, 30,  clf)
+    
+    return rect
